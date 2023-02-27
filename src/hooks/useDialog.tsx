ba6609cb00e1e27ca6a25f05fa1439c15c4d7f02
oscelest <oscelest@gmail.com>
2023-02-27 23:14:15 +0100

    Updates to packages.

--- src/hooks/useDialog.tsx
+++ src/hooks/useDialog.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import {Dialog} from "../classes";
 import {DialogInstance, DialogInstanceProps} from "../components";
 import {DialogContext} from "../index";
